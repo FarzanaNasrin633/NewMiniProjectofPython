1a788974fb679d3e0a2c589b6f39855db7e38775
FarzanaNasrin633 <fnasrin633@gmail.com>
2022-10-24 12:22:56 +0600

    added new file

--- numberguess.py
+++ numberguess.py
@@ -1,34 +1,41 @@
+from curses import is_term_resized
+from curses.ascii import isdigit
 import random
+ 
+input_number = input("Type a number: ")
 
-top_of_range = input("type a number: ")
+if input_number.isdigit():
+    input_number = int(input_number)
 
-if top_of_range.isdigit():
-    top_of_range = int (top_of_range)
-
-    if top_of_range <= 0:
-        print('Please type a number larger trhen 0 next time.')
+    if input_number == 0:
+        print("you should enter a number more than 0")
         quit()
 
-    else:
-        print('Please type a number next time.')
-        quit()
+    random_number = random.randrange(2, input_number)
 
+else:
+    print("You didnt input a number")
+    quit()
 
-
-
-
-random_number = random.randint(0, top_of_range)
-print(random_number)
 
 while True:
-    user_guess = input("Make a guess: ")
-    if user_guess.isdigit():
-        user_guess = int(user_guess)
+    random_input_number = input("guess a number: ")
 
-    if top_of_range <= 0;
-        print('Please type larger than 0 next time.')
-        quit()
+    if random_input_number.isdigit():
+        random_input_number = int(random_input_number)
+        
+        if random_number == random_input_number:
+            print("You got it, yeahhh 🎆🎉🎉")
+            break
+
+        elif random_input_number > random_number:
+            print("Your guess is bigger than the actural number, try again 😀")
+            continue
+
+        else:
+            print("Your guess is smaller than the actural number, try again 😀")
+            continue
 
     else:
-        print('Please')
-    
+        print("You didnt input a number")
+        continue
